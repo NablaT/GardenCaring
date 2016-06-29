961c1fc81d37bc5686c2382e1a298cf62fc0c688
NablaT <remi.pourtier@gmail.com>
2016-06-29 16:55:37 +0200

    Starting about component

--- src/app/components/+about/about.component.ts
+++ src/app/components/+about/about.component.ts
@@ -1,12 +1,42 @@
 import {Component} from "@angular/core";
 import {CardComponent} from "../core/card/card.component";
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
+import {LandApi} from "../../shared/services/src/land-api.service";
+import {Plot} from "../../shared/models/Plot";
 
 @Component({
     selector: 'about',
     moduleId: module.id,
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css'],
-    directives: [CardComponent]
+    providers: [LandApi],
+    directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class AboutComponent {
+
+    public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    public doughnutChartData:number[] = [350, 450, 100, 100];
+    public doughnutChartType:string = 'doughnut';
+
+    public plots:Array<Plot>;
+
+    constructor(private landService:LandApi) {
+        this.plots=[];
+    }
+
+    ngOnInit(){
+        this.landService.findPlots().subscribe(
+            plots=>this.plots = plots
+        );
+    }
+
+    // events
+    public chartClicked(e:any):void {
+
+    }
+
+    public chartHovered(e:any):void {
+        console.log(e);
+    }
 }
